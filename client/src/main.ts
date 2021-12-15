f039bba0ca328b0a544bbce5515c03ace75f3a98
mazrean <shunsuke.w04@gmail.com>
2021-12-15 14:02:15 +0900

    bootstrap導入

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,8 @@ import router from './router'
 import store from './store'
 import App from './App.vue'
 import './index.scss'
+import 'bootstrap'
+import 'bootstrap/scss/bootstrap.scss'
 
 const app = createApp(App)
 app.use(router)
--- client/src/pages/Hello.vue
+++ client/src/pages/Hello.vue
@@ -2,6 +2,29 @@
   <routes />
   <div>Hello</div>
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <!--Bootstrap test-->
+  <div class="dropdown">
+    <button
+      id="dropdownMenuButton1"
+      class="btn btn-secondary dropdown-toggle"
+      type="button"
+      data-bs-toggle="dropdown"
+      aria-expanded="false"
+    >
+      Check Bootstrap
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <li>
+        <a class="dropdown-item" href="#">Action</a>
+      </li>
+      <li>
+        <a class="dropdown-item" href="#">Another action</a>
+      </li>
+      <li>
+        <a class="dropdown-item" href="#">Something else here</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
