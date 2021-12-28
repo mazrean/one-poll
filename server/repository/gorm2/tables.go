6b271ef9a9b3b61e76bbc334351a99dfefcb4e6a
mazrean <shunsuke.w04@gmail.com>
2021-12-28 20:52:56 +0900

    usersテーブル追加

--- server/repository/gorm2/tables.go
+++ server/repository/gorm2/tables.go
@@ -1,5 +1,19 @@
 package gorm2
 
+import "github.com/google/uuid"
+
 var (
-	tables = []interface{}{}
+	tables = []interface{}{
+		&UserTable{},
+	}
 )
+
+type UserTable struct {
+	ID       uuid.UUID `gorm:"type:char(36);not null;primaryKey;size:36"`
+	Name     string    `gorm:"type:varchar(50);not null;size:50"`
+	Password string    `gorm:"type:char(60);not null;size:60"`
+}
+
+func (*UserTable) TableName() string {
+	return "users"
+}
