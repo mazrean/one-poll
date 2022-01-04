f66bb26ece0337576273a38bdb4ebb0701588841
mazrean <shunsuke.w04@gmail.com>
2022-01-04 17:37:55 +0900

    POST /users/signin実装

--- server/handler/v1/user.go
+++ server/handler/v1/user.go
@@ -60,3 +60,52 @@ func (u *User) PostUsers(c echo.Context) error {
 
 	return c.NoContent(http.StatusCreated)
 }
+
+func (u *User) PostUsersSignin(c echo.Context) error {
+	var userRequest openapi.PostUsersSigninJSONRequestBody
+	err := c.Bind(&userRequest)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid json body")
+	}
+
+	name := values.NewUserName(string(userRequest.Name))
+	err = name.Validate()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid user name")
+	}
+
+	password := values.NewUserPassword([]byte(userRequest.Password))
+	err = password.Validate()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid user password")
+	}
+
+	user, err := u.authorizationService.Login(
+		c.Request().Context(),
+		name,
+		password,
+	)
+	if errors.Is(err, service.ErrInvalidUserOrPassword) {
+		return echo.NewHTTPError(http.StatusUnauthorized, "invalid user or password")
+	}
+	if err != nil {
+		log.Printf("failed to signin: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to signin")
+	}
+
+	session, err := u.Session.getSession(c)
+	if err != nil {
+		log.Printf("failed to get session: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get session")
+	}
+
+	u.Session.setUser(session, user)
+
+	err = u.Session.save(c, session)
+	if err != nil {
+		log.Printf("failed to save session: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to save session")
+	}
+
+	return c.NoContent(http.StatusOK)
+}
