9a66ef7ef58e31380747e2abd064bd9b7863bbcb
series2 <series2.others@gmail.com>
2022-01-08 22:51:23 +0900

    get comments の実装概形

---
+++ server/handler/v1/comment.go
@@ -0,0 +1,24 @@
+package v1
+
+import (
+	"net/http"
+	"time"
+
+	openapi "github.com/cs-sysimpl/suzukake/handler/v1/openapi"
+	"github.com/labstack/echo/v4"
+)
+
+type Comment struct {
+}
+
+// [TODO] service の GetComments を使う
+func (c Comment) GetPollsPollIDComments(ctx echo.Context, pollID string, params openapi.GetPollsPollIDCommentsParams) error {
+
+	return ctx.JSON(http.StatusOK, []openapi.PollComment{
+		{	
+			Content:   "content",
+			CreatedAt: time.Now(),
+			User:      openapi.User{Name: "hoge", Uuid: "uuid"},
+		},
+	})
+}
