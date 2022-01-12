85ec5604554927201ca2d3b0b583ac5146c81d38
mazrean <shunsuke.w04@gmail.com>
2022-01-12 19:18:47 +0900

    responseリポジトリにGetResponseByUserAndPollID追加

--- server/repository/response.go
+++ server/repository/response.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"time"
 
 	"github.com/cs-sysimpl/suzukake/domain"
 	"github.com/cs-sysimpl/suzukake/domain/values"
@@ -9,6 +10,10 @@ import (
 
 type Response interface {
 	GetResponsesByPollID(ctx context.Context, pollID values.PollID) ([]*domain.Response, error)
+	GetResponseByUserAndPollID(ctx context.Context, userID values.UserID, pollID values.PollID, lockType LockType) (*domain.Response, error)
+	GetIDsByPollID(ctx context.Context, pollID values.PollID) ([]*values.ResponseID, error)
+	GetCreatedAtByID(ctx context.Context, id values.ResponseID) (time.Time, error)
+	GetUserIDByID(ctx context.Context, id values.ResponseID) (values.UserID, error)
 }
 
 type ResponseInfo struct {
