94fe817e36d11155371c9a76578fe519d128e178
mazrean <shunsuke.w04@gmail.com>
2022-01-12 20:30:10 +0900

    CanRead実装

--- server/service/v1/poll_authority.go
+++ server/service/v1/poll_authority.go
@@ -1,6 +1,11 @@
 package v1
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/cs-sysimpl/suzukake/domain"
 	"github.com/cs-sysimpl/suzukake/repository"
 )
 
@@ -12,4 +17,20 @@ func NewPollAuthority(responseRepository repository.Response) *PollAuthority {
 	return &PollAuthority{
 		responseRepository: responseRepository,
 	}
+}
+
+func (p *PollAuthority) CanRead(ctx context.Context, user *domain.User, poll *domain.Poll) (bool, error) {
+	if poll.IsExpired() {
+		return true, nil
+	}
+
+	_, err := p.responseRepository.GetResponseByUserAndPollID(ctx, user.GetID(), poll.GetID(), repository.LockTypeNone)
+	if errors.Is(err, repository.ErrRecordNotFound) {
+		return true, nil
+	}
+	if err != nil {
+		return false, fmt.Errorf("failed to get response: %w", err)
+	}
+
+	return false, nil
 }
