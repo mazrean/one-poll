0392390ff9418b4e2bc8e4be00919fc6c085f52d
mazrean <shunsuke.w04@gmail.com>
2022-01-12 19:25:08 +0900

    Responseリポジトリのinterface修正

--- server/repository/response.go
+++ server/repository/response.go
@@ -2,13 +2,16 @@ package repository
 
 import (
 	"context"
-	"time"
 
+	"github.com/cs-sysimpl/suzukake/domain"
 	"github.com/cs-sysimpl/suzukake/domain/values"
 )
 
 type Response interface {
-	GetIDsByPollID(ctx context.Context, pollID values.PollID) ([]*values.ResponseID, error)
-	GetCreatedAtByID(ctx context.Context, id values.ResponseID) (time.Time, error)
-	GetUserIDByID(ctx context.Context, id values.ResponseID) (values.UserID, error)
+	GetResponsesByPollID(ctx context.Context, pollID values.PollID) ([]*domain.Response, error)
+}
+
+type ResponseInfo struct {
+	*domain.Response
+	Respondent *domain.User
 }
