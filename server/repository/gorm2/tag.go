5aee5182579de21399717023797edb1d0ecd115f
mazrean <shunsuke.w04@gmail.com>
2022-01-15 22:22:02 +0900

    GetTagsByName実装

--- server/repository/gorm2/tag.go
+++ server/repository/gorm2/tag.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	"github.com/cs-sysimpl/suzukake/domain"
+	"github.com/cs-sysimpl/suzukake/domain/values"
+	"github.com/cs-sysimpl/suzukake/repository"
 	"github.com/google/uuid"
 )
 
@@ -34,4 +36,40 @@ func (t *Tag) CreateTag(ctx context.Context, tag *domain.Tag) error {
 	}
 
 	return nil
+}
+
+func (t *Tag) GetTagsByName(ctx context.Context, names []values.TagName, lockType repository.LockType) ([]*domain.Tag, error) {
+	db, err := t.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	db, err = t.db.setLock(db, lockType)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set lock: %w", err)
+	}
+
+	strNames := make([]string, 0, len(names))
+	for _, name := range names {
+		strNames = append(strNames, string(name))
+	}
+
+	var tagTables []TagTable
+	err = db.
+		Where("name IN ?", strNames).
+		Select("id", "name").
+		Find(&tagTables).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get tags: %w", err)
+	}
+
+	tags := make([]*domain.Tag, 0, len(tagTables))
+	for _, tagTable := range tagTables {
+		tags = append(tags, domain.NewTag(
+			values.NewTagIDFromUUID(tagTable.ID),
+			values.NewTagName(tagTable.Name),
+		))
+	}
+
+	return tags, nil
 }
