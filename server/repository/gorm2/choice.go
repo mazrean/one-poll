fbd2130909ad2493e970aa540413b6384384017c
mazrean <shunsuke.w04@gmail.com>
2022-01-15 21:26:10 +0900

    GetChoices実装

--- server/repository/gorm2/choice.go
+++ server/repository/gorm2/choice.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cs-sysimpl/suzukake/domain"
 	"github.com/cs-sysimpl/suzukake/domain/values"
+	"github.com/cs-sysimpl/suzukake/repository"
 	"github.com/google/uuid"
 )
 
@@ -40,4 +41,35 @@ func (c *Choice) CreateChoices(ctx context.Context, pollID values.PollID, choice
 	}
 
 	return nil
+}
+
+func (c *Choice) GetChoices(ctx context.Context, ids []values.ChoiceID, lockType repository.LockType) ([]*domain.Choice, error) {
+	db, err := c.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	uuidChoiceIDs := make([]uuid.UUID, 0, len(ids))
+	for _, id := range ids {
+		uuidChoiceIDs = append(uuidChoiceIDs, uuid.UUID(id))
+	}
+
+	var choiceTables []ChoiceTable
+	err = db.
+		Where("id IN ?", uuidChoiceIDs).
+		Select("id", "name").
+		Find(&choiceTables).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get choices: %w", err)
+	}
+
+	choices := make([]*domain.Choice, 0, len(choiceTables))
+	for _, choiceTable := range choiceTables {
+		choices = append(choices, domain.NewChoice(
+			values.NewChoiceIDFromUUID(choiceTable.ID),
+			values.NewChoiceLabel(choiceTable.Name),
+		))
+	}
+
+	return choices, nil
 }
