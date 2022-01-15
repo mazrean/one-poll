56207e280f0b72b467e8a7ff70c328ea5422525e
mazrean <shunsuke.w04@gmail.com>
2022-01-15 21:22:11 +0900

    CreateChoices実装

--- server/repository/gorm2/choice.go
+++ server/repository/gorm2/choice.go
@@ -1,5 +1,14 @@
 package gorm2
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/cs-sysimpl/suzukake/domain"
+	"github.com/cs-sysimpl/suzukake/domain/values"
+	"github.com/google/uuid"
+)
+
 type Choice struct {
 	db *DB
 }
@@ -8,4 +17,27 @@ func NewChoice(db *DB) *Choice {
 	return &Choice{
 		db: db,
 	}
+}
+
+func (c *Choice) CreateChoices(ctx context.Context, pollID values.PollID, choices []*domain.Choice) error {
+	db, err := c.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	choiceTables := make([]ChoiceTable, 0, len(choices))
+	for _, choice := range choices {
+		choiceTables = append(choiceTables, ChoiceTable{
+			ID:     uuid.UUID(choice.GetID()),
+			PollID: uuid.UUID(pollID),
+			Name:   string(choice.GetLabel()),
+		})
+	}
+
+	err = db.Create(&choiceTables).Error
+	if err != nil {
+		return fmt.Errorf("failed to create choices: %w", err)
+	}
+
+	return nil
 }
