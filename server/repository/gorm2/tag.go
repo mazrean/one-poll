b8803a575e25627f487c91409e248b9c775fdd07
mazrean <shunsuke.w04@gmail.com>
2022-01-15 22:15:51 +0900

    CreateTag実装

--- server/repository/gorm2/tag.go
+++ server/repository/gorm2/tag.go
@@ -1,5 +1,13 @@
 package gorm2
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/cs-sysimpl/suzukake/domain"
+	"github.com/google/uuid"
+)
+
 type Tag struct {
 	db *DB
 }
@@ -8,4 +16,22 @@ func NewTag(db *DB) *Tag {
 	return &Tag{
 		db: db,
 	}
+}
+
+func (t *Tag) CreateTag(ctx context.Context, tag *domain.Tag) error {
+	db, err := t.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	tagTable := TagTable{
+		ID:   uuid.UUID(tag.GetID()),
+		Name: string(tag.GetName()),
+	}
+	err = db.Create(&tagTable).Error
+	if err != nil {
+		return fmt.Errorf("failed to create tag: %w", err)
+	}
+
+	return nil
 }
