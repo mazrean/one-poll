62c7fe58253192b9e7cb2e471761c0930abc5198
series2 <series2.others@gmail.com>
2022-01-17 00:11:04 +0900

    Implemention Get polls/{pollid}/comments

--- server/handler/v1/comment.go
+++ server/handler/v1/comment.go
@@ -1,24 +1,48 @@
 package v1
 
 import (
+	"log"
 	"net/http"
-	"time"
 
+	"github.com/cs-sysimpl/suzukake/domain/values"
 	openapi "github.com/cs-sysimpl/suzukake/handler/v1/openapi"
+	"github.com/cs-sysimpl/suzukake/service"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
 type Comment struct {
+	commentService service.Comment
 }
 
-// [TODO] service の GetComments を使う
+// todo Comment の数の制限については未実装
 func (c Comment) GetPollsPollIDComments(ctx echo.Context, pollID string, params openapi.GetPollsPollIDCommentsParams) error {
+	uuidPollID, err := uuid.Parse(pollID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid poll id")
+	}
+	commentInfos, err := c.commentService.GetComments(
+		ctx.Request().Context(),
+		values.NewPollIDFromUUID(uuidPollID),
+	)
+	if err != nil {
+		log.Printf("failed to get comments: %v", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get comments")
+	}
 
-	return ctx.JSON(http.StatusOK, []openapi.PollComment{
-		{
-			Content:   "content",
-			CreatedAt: time.Now(),
-			User:      openapi.User{Name: "hoge", Uuid: "uuid"},
-		},
-	})
+	comments := make([]openapi.PollComment, 0, len(commentInfos))
+
+	for _, commentInfo := range commentInfos {
+		comment := openapi.PollComment{
+			Content:   string(commentInfo.GetContent()),
+			CreatedAt: commentInfo.GetCreatedAt(),
+			User: openapi.User{
+				Name: openapi.UserName(commentInfo.CommentUser.GetName()),
+				Uuid: uuid.UUID(commentInfo.CommentUser.GetID()).String(),
+			},
+		}
+		comments = append(comments, comment)
+	}
+
+	return ctx.JSON(http.StatusOK, comments)
 }
