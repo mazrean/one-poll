b5f366ad65d25aba591b1f5448b0120827cb7e76
series2 <series2.others@gmail.com>
2022-01-18 23:22:52 +0900

    add authorized check (get comment)

--- server/handler/v1/comment.go
+++ server/handler/v1/comment.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
@@ -12,13 +13,16 @@ import (
 )
 
 type Comment struct {
+	*Session
 	commentService service.Comment
 }
 
 func NewComment(
+	session *Session,
 	commentService service.Comment,
 ) *Comment {
 	return &Comment{
+		Session:        session,
 		commentService: commentService,
 	}
 }
@@ -29,6 +33,20 @@ func (c Comment) GetPollsPollIDComments(ctx echo.Context, pollID string, params 
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "invalid poll id")
 	}
+
+	session, err := c.Session.getSession(ctx)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid session")
+	}
+
+	_, err = c.Session.getUser(session)
+	if errors.Is(err, ErrNoValue) {
+		return echo.NewHTTPError(http.StatusUnauthorized, "login required")
+	}
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get user")
+	}
+
 	commentInfos, err := c.commentService.GetComments(
 		ctx.Request().Context(),
 		values.NewPollIDFromUUID(uuidPollID),
