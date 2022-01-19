d77bc94cb8e89ee829d3a6ae48851f9c34c0aa7c
series2 <series2.others@gmail.com>
2022-01-20 00:47:57 +0900

    宗教戦争が起きかけたので鎮圧(ポインタについてもとに戻した)

--- server/repository/poll.go
+++ server/repository/poll.go
@@ -13,7 +13,7 @@ type Poll interface {
 	UpdatePollDeadline(ctx context.Context, id values.PollID, deadline sql.NullTime) error
 	// GetPollsメソッドは Get polls,Get /users/me/owners Get/users/me/answers での仕様を想定
 	// 現状のクライアントからの利用方法的に、一度にすべてのパラメタが埋まることはないが、仕様としてはパラメタ条件のAndをとる。
-	GetPolls(ctx context.Context, params PollSearchParams) ([]*PollInfo, error)
+	GetPolls(ctx context.Context, params *PollSearchParams) ([]*PollInfo, error)
 	GetPoll(ctx context.Context, id values.PollID, lockType LockType) (*PollInfo, error)
 	DeletePoll(ctx context.Context, id values.PollID) error
 	AddTags(ctx context.Context, pollID values.PollID, tags []values.TagID) error
@@ -24,12 +24,10 @@ type PollInfo struct {
 	Owner *domain.User
 }
 
-// パラメタを指定していない要素については、その要素へのポインタをnilにする。
-// 例: Matchに空文字が指定されていても、nilではないので、条件として働きます。
 type PollSearchParams struct {
-	Limit  *int
-	Offset *int
-	Match  *string
-	Owner  *domain.User
-	Answer *domain.User
+	Limit  int // default値(0)の時、指定なし
+	Offset int // default値(0)の時、指定なし
+	Match  string //default値("")の時、指定なし
+	Owner  *domain.User // nilの時指定なし
+	Answer *domain.User // nilの時指定なし
 }
