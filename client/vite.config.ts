e0b425690a9d299b29d741dc5a865ce7457b3e4e
mazrean <shunsuke.w04@gmail.com>
2022-01-27 13:48:44 +0900

    brotli圧縮

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import brotli from "rollup-plugin-brotli";
 
 export default defineConfig({
   resolve: {
@@ -17,5 +18,14 @@ export default defineConfig({
       }
     }
   },
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        brotli({
+          test: /\.(js|css|html|svg)$/
+        })
+      ]
+    }
+  }
 })
