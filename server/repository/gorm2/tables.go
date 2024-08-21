700a54d87f774c9b942d042fa7025fe82c2acce3
mazrean <shunsuke.w04@gmail.com>
2024-08-21 15:54:17 +0900

    create webauthn table

--- server/repository/gorm2/tables.go
+++ server/repository/gorm2/tables.go
@@ -11,6 +11,10 @@ import (
 var (
 	tables = []interface{}{
 		&UserTable{},
+		&WebAuthnCredentialTable{},
+		&WebAuthnCredentialAlgorithmTable{},
+		&WebAuthnCredentialTransportTypeTable{},
+		&WebAuthnCredentialTransportTable{},
 		&PollTable{},
 		&PollTypeTable{},
 		&TagTable{},
@@ -28,6 +32,53 @@ type UserTable struct {
 
 func (*UserTable) TableName() string {
 	return "users"
+}
+
+type WebAuthnCredentialTable struct {
+	ID          uuid.UUID                          `gorm:"type:char(36);not null;primaryKey;size:36"`
+	UserID      uuid.UUID                          `gorm:"type:char(36);not null;size:36"`
+	CredID      []byte                             `gorm:"type:binary(64);not null;size:64"`
+	Name        string                             `gorm:"type:varchar(50);not null;size:50"`
+	PublicKey   []byte                             `gorm:"type:binary(65);not null;size:65"`
+	AlgorithmID int                                `gorm:"type:tinyint;not null"`
+	Algorithm   WebAuthnCredentialAlgorithmTable   `gorm:"foreignKey:AlgorithmID"`
+	Transports  []WebAuthnCredentialTransportTable `gorm:"foreignKey:CredentialID"`
+	CreatedAt   time.Time                          `gorm:"type:datetime;not null"`
+	LastUsedAt  time.Time                          `gorm:"type:datetime;not null"`
+}
+
+func (*WebAuthnCredentialTable) TableName() string {
+	return "webauthn_credentials"
+}
+
+type WebAuthnCredentialAlgorithmTable struct {
+	ID     int    `gorm:"type:TINYINT AUTO_INCREMENT;not null;primaryKey"`
+	Name   string `gorm:"type:varchar(50);not null;size:50;unique"`
+	Active bool   `gorm:"type:bool;not null;default:true"`
+}
+
+func (*WebAuthnCredentialAlgorithmTable) TableName() string {
+	return "webauthn_credential_algorithms"
+}
+
+type WebAuthnCredentialTransportTypeTable struct {
+	ID     int    `gorm:"type:TINYINT AUTO_INCREMENT;not null;primaryKey"`
+	Name   string `gorm:"type:varchar(50);not null;size:50;unique"`
+	Active bool   `gorm:"type:bool;not null;default:true"`
+}
+
+func (*WebAuthnCredentialTransportTypeTable) TableName() string {
+	return "webauthn_credential_transport_types"
+}
+
+type WebAuthnCredentialTransportTable struct {
+	CredentialID uuid.UUID `gorm:"type:char(36);not null;primaryKey;size:36"`
+	TypeID       int       `gorm:"type:TINYINT;not null;primaryKey"`
+	Type         WebAuthnCredentialTransportTypeTable
+}
+
+func (*WebAuthnCredentialTransportTable) TableName() string {
+	return "webauthn_credential_transports"
 }
 
 type PollTable struct {
