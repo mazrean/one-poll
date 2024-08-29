4c9d8e03d98a5950ce7027590ff554f85a14e2fb
mazrean <shunsuke.w04@gmail.com>
2024-08-29 20:27:55 +0900

    コンテナを1つに統合

--- server/handler/v1/api.go
+++ server/handler/v1/api.go
@@ -4,7 +4,11 @@ package v1
 //go:generate go fmt ./openapi/openapi.gen.go
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
+	"regexp"
+	"strings"
 
 	"github.com/getkin/kin-openapi/openapi3filter"
 	"github.com/labstack/echo/v4"
@@ -12,6 +16,9 @@ import (
 	openapi "github.com/mazrean/one-poll/handler/v1/openapi"
 	oapiMiddleware "github.com/oapi-codegen/echo-middleware"
 )
+
+//go:embed static/*
+var staticFS embed.FS
 
 type unimplemented interface {
 	// (POST /tags)
@@ -59,14 +66,65 @@ func (a *API) Start(addr string) error {
 
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
-	e.Use(oapiMiddleware.OapiRequestValidatorWithOptions(swagger, &oapiMiddleware.Options{
+	e.Use(middleware.RewriteWithConfig(middleware.RewriteConfig{
+		Rules: map[string]string{
+			"/":        "/index.html",
+			"/signup":  "/index.html",
+			"singin":   "/index.html",
+			"/profile": "/index.html",
+		},
+		RegexRules: map[*regexp.Regexp]string{
+			regexp.MustCompile("^/details/(.*)$"): "/index.html",
+		},
+	}))
+	e.Use(StaticBrotliMiddleware)
+
+	staticFS, err := fs.Sub(staticFS, "static")
+	if err != nil {
+		return fmt.Errorf("failed to get staticFS: %w", err)
+	}
+	e.StaticFS("/", staticFS)
+
+	api := e.Group("/api", oapiMiddleware.OapiRequestValidatorWithOptions(swagger, &oapiMiddleware.Options{
 		Options: openapi3filter.Options{
 			MultiError:         true,
 			AuthenticationFunc: a.Checker.check,
 		},
 	}))
-
-	openapi.RegisterHandlersWithBaseURL(e, a, "/api")
+	openapi.RegisterHandlersWithBaseURL(api, a, "")
 
 	return e.Start(addr)
+}
+
+func StaticBrotliMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if c.Request().Method != "GET" ||
+			strings.HasPrefix(c.Path(), "/api") {
+			return next(c)
+		}
+
+		exts := []string{".html", ".css", ".js"}
+		for _, ext := range exts {
+			if strings.HasSuffix(c.Path(), ext) {
+				goto AFTER_EXT_LOOP
+			}
+		}
+		return next(c)
+
+	AFTER_EXT_LOOP:
+		acceptEncodings := strings.Split(strings.TrimSpace(c.Request().Header.Get("Accept-Encoding")), ",")
+		for _, acceptEncoding := range acceptEncodings {
+			encoding, _, _ := strings.Cut(acceptEncoding, ";")
+			if encoding == "br" {
+				goto AFTER_ACCEPT_ENCODING_LOOP
+			}
+		}
+		return next(c)
+
+	AFTER_ACCEPT_ENCODING_LOOP:
+		c.Response().Header().Set("Content-Encoding", "br")
+		c.Request().URL.Path += ".br"
+
+		return next(c)
+	}
 }
