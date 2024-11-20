34117f111cd0efa764dffe8c2673120c730a81d0
mazrean <shunsuke.w04@gmail.com>
2024-11-20 18:58:00 +0900

    sidemenuのレスポンシブ対応

--- client/src/components/SideMenuNarrow.vue
+++ client/src/components/SideMenuNarrow.vue
@@ -1,32 +1,26 @@
 <template>
   <div
-    class="d-flex flex-column flex-shrink-0 p-3 bg-light vh-100"
-    style="width: 90px; position: sticky; top: 0">
-    <h1 class="display-5 py-3 px-0">
-      <router-link
-        class="link"
-        :to="{ name: 'home' }"
-        style="font-weight: normal"
-        ><svg
+    class="d-flex flex-column flex-shrink-0 px-2 py-3 bg-light vh-100"
+    style="min-width: 60px; width: 10%; position: sticky; top: 0">
+    <h1 class="display-5 px-0 m-0">
+      <router-link class="link" :to="{ name: 'home' }">
+        <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
           fill="currentColor"
           class="bi bi-tree-fill"
           viewBox="0 0 16 16">
           <path
-            d="M8.416.223a.5.5 0 0 0-.832 0l-3 4.5A.5.5 0 0 0 5 5.5h.098L3.076 8.735A.5.5 0 0 0 3.5 9.5h.191l-1.638 3.276a.5.5 0 0 0 .447.724H7V16h2v-2.5h4.5a.5.5 0 0 0 .447-.724L12.31 9.5h.191a.5.5 0 0 0 .424-.765L10.902 5.5H11a.5.5 0 0 0 .416-.777l-3-4.5z" /></svg
-      ></router-link>
+            d="M8.416.223a.5.5 0 0 0-.832 0l-3 4.5A.5.5 0 0 0 5 5.5h.098L3.076 8.735A.5.5 0 0 0 3.5 9.5h.191l-1.638 3.276a.5.5 0 0 0 .447.724H7V16h2v-2.5h4.5a.5.5 0 0 0 .447-.724L12.31 9.5h.191a.5.5 0 0 0 .424-.765L10.902 5.5H11a.5.5 0 0 0 .416-.777l-3-4.5z" />
+        </svg>
+      </router-link>
     </h1>
     <hr />
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <h4>
-          <router-link class="nav-link link px-0" :to="{ name: 'home' }">
+    <ul class="nav nav-pills flex-column mb-auto gap-3">
+      <li class="nav-item p-0 m-0 w-100">
+        <h4 class="m-0">
+          <router-link class="nav-link link p-0 m-0" :to="{ name: 'home' }">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
               fill="currentColor"
               class="bi bi-house-fill"
               viewBox="0 0 16 16">
@@ -35,17 +29,16 @@
                 d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
               <path
                 fill-rule="evenodd"
-                d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" /></svg
-          ></router-link>
+                d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+            </svg>
+          </router-link>
         </h4>
       </li>
-      <li v-if="userID">
-        <h4>
-          <router-link class="nav-link link px-0" :to="{ name: 'profile' }">
+      <li v-if="userID" class="nav-item p-0 m-0 w-100">
+        <h4 class="m-0">
+          <router-link class="nav-link link p-0 m-0" :to="{ name: 'profile' }">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
               fill="currentColor"
               class="bi bi-person-fill"
               viewBox="0 0 16 16">
